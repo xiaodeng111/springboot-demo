1d1af1dc1bbcb7067d568b01584237b15f147221
dengrenqian <deng@163.com>
2018-05-29 09:38:31 +0800

    删除不必要文件

--- clockin/src/main/java/com/demo/clockin/ClockinApplication.java
+++ clockin/src/main/java/com/demo/clockin/ClockinApplication.java
@@ -3,6 +3,7 @@ package com.demo.clockin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -10,11 +11,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAutoConfiguration//自动载入应用程序所需的所有Bean，当使用Exclude这个属性时，是禁止自动配置某个类
-@ComponentScan
+@SpringBootApplication
+//@EnableAutoConfiguration//自动载入应用程序所需的所有Bean，当使用Exclude这个属性时，是禁止自动配置某个类
+//@ComponentScan
 @ServletComponentScan //加载注解@WebServlet, @WebFilter, and @WebListener
 @MapperScan("com.demo.clockin.dao")
-public class ClockinApplication extends SpringBootServletInitializer {
+public class ClockinApplication extends SpringBootServletInitializer {//SpringBootServletInitializer加载解析web.xml应用的filter,listener,interceptor
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
