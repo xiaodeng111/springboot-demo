36e4306df8f663a10419b388cbb1296ade6d0037
dengrenqian <deng@163.com>
2018-05-29 14:47:09 +0800

    使用springboot内嵌tomcat启动

--- clockin/src/main/java/com/demo/clockin/ClockinApplication.java
+++ clockin/src/main/java/com/demo/clockin/ClockinApplication.java
@@ -12,8 +12,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SpringBootApplication
-//@EnableAutoConfiguration//自动载入应用程序所需的所有Bean，当使用Exclude这个属性时，是禁止自动配置某个类
-//@ComponentScan
 @ServletComponentScan //加载注解@WebServlet, @WebFilter, and @WebListener
 @MapperScan("com.demo.clockin.dao")
 public class ClockinApplication extends SpringBootServletInitializer {//SpringBootServletInitializer加载解析web.xml应用的filter,listener,interceptor
