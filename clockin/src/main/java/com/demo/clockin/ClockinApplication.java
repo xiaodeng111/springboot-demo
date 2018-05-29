1511fe32b0a1a7c1c1503b069a933e2d810ea0d6
dengrenqian <deng@163.com>
2018-05-29 14:54:19 +0800

    SpringBootApplication的作用

--- clockin/src/main/java/com/demo/clockin/ClockinApplication.java
+++ clockin/src/main/java/com/demo/clockin/ClockinApplication.java
@@ -10,8 +10,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@SpringBootApplication
+@SpringBootApplication//等同于@Configuration @EnableAutoConfiguration @ComponentScan
 @ServletComponentScan //加载注解@WebServlet, @WebFilter, and @WebListener
 @MapperScan("com.demo.clockin.dao")
 public class ClockinApplication extends SpringBootServletInitializer {//SpringBootServletInitializer加载解析web.xml应用的filter,listener,interceptor
